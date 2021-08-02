258e3682f4fdad70ac7cf2212d50fa39c98bc1b5
Jason Robinson <json.robinson@gmail.com>
2021-08-02 13:59:45 -0500

    Importing only the functions needed.

--- src/packages.lisp
+++ src/packages.lisp
@@ -15,7 +15,17 @@
   (:documentation "Handles all the user interfaces and actions."))
 
 (defpackage processing-io
-  (:use :cl :model :view :iup)
+  (:use :cl)
+  (:import-from :model
+		#:process-greeting
+		#:process-input-string)
+  (:import-from :view
+		#:get-name
+		#:greeting
+		#:get-input-string
+		#:set-output-string
+		#:get-quote-and-author
+		#:display-quote)
   (:export #:saying-hello
 	   #:counting-chars
 	   #:run-count-chars
